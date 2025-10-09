9a195eb3d8ea03c319ab51d5bb692b6160d32398
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-09 10:15:20 -0700

    Add loading state to register flow

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -2,10 +2,10 @@ import type { Route } from "./+types/register";
 
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import {Link} from "react-router";
+import { Link } from "react-router";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
-import {Form} from "react-router";
-import {useNavigate} from "react-router";
+import { Form } from "react-router";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,78 +15,149 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Register() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const auth = getAuth();
-    const db = getFirestore();
-    const navigate = useNavigate();
-    // Function to register a new user and create their profile in Firestore
-    async function registerAndCreateProfile(firstName: string, lastName: string, email: string, password: string) {
-      try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-    
-        // Create a user profile document in Firestore using the user's UID
-        await setDoc(doc(db, "users", user.uid), {
-          uid: user.uid,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          createdAt: new Date(),
-          // Add other relevant profile information
-        });
-    
-        console.log("User registered and profile created:", user.uid);
-        navigate("/");
+  const [isLoading, setIsLoading] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = getAuth();
+  const db = getFirestore();
+  const navigate = useNavigate();
+  // Function to register a new user and create their profile in Firestore
+  async function registerAndCreateProfile(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+  ) {
+    try {
+      setIsLoading(true);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
 
-      } catch (error: any) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error("Error during registration or profile creation:", errorCode, errorMessage);
-        throw error; // Re-throw the error for handling in your application
-      }
+      // Create a user profile document in Firestore using the user's UID
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        createdAt: new Date(),
+        workouts: [],
+        // Add other relevant profile information
+      });
+
+      console.log("User registered and profile created:", user.uid);
+      navigate("/");
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(
+        "Error during registration or profile creation:",
+        errorCode,
+        errorMessage
+      );
+      throw error; // Re-throw the error for handling in your application
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    return (
-        <div>
-          <h1>Register</h1>
-            <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={(e) => registerAndCreateProfile(firstName, lastName, email, password)}>
-                <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
-                    First Name
-                </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="firstName" type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                </div>
-                <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
-                    Last Name
-                </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="lastName" type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                </div>
-                <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                    Email
-                </label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                    Password
-                </label>
-                <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <p className="text-red-500 text-xs italic">Please choose a password.</p>
-                </div>
-                <div className="flex items-center justify-between">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    Sign Up
-                </button>
-                <Link to="/" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-                    Already have an account?{' '}
-                </Link>
-                </div>
-            </Form>
+  return (
+    <div>
+      <h1>Register</h1>
+      <Form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={(e) =>
+          registerAndCreateProfile(firstName, lastName, email, password)
+        }
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="firstName"
+          >
+            First Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="firstName"
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
-    )
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="lastName"
+          >
+            Last Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="lastName"
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className={`${isLoading ? "bg-gray-500" : "bg-blue-500 hover:bg-blue-700"} text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? "Signing up..." : "Sign up"}
+          </button>
+          <Link
+            to="/"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          >
+            Already have an account?{" "}
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
 }
