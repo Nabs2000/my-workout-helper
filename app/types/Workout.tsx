ff9946414593fd65f4276e948851274ad5ca94a8
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-10 23:03:41 -0700

    Account for the input form and Google response

--- app/types/Workout.tsx
+++ app/types/Workout.tsx
@@ -1,9 +1,10 @@
 import type { WorkoutType } from "./workoutType";
+import { Timestamp } from "firebase/firestore";
 
 export interface Workout {
   workoutName: WorkoutType;
   numberSets: number;
   numberReps: number;
   weight: number;
-  dateLogged: Date;
+  dateLogged: Date | Timestamp;
 }
