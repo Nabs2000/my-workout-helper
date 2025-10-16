ef15eefa4cbe1c9cb9d0de9381e8bb86a876c0a2
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-16 10:57:19 -0700

    Start building weight graph component

---
+++ app/components/weightProgGraph.tsx
@@ -0,0 +1,9 @@
+import type { User } from "~/types/User";
+
+export default function WeightProgGraph({ user }: { user: User }) {
+    return (
+        <div>
+            <h1>Weight Progress Graph</h1>
+        </div>
+    );
+}
