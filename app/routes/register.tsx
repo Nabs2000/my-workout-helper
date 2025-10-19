b502b92cb86ad96b450b45ab53e86d712a03013a
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-18 18:25:40 -0700

    Remove red outline

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -131,16 +131,13 @@ export default function Register() {
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
         </div>
         <div className="flex items-center justify-between">
           <button
