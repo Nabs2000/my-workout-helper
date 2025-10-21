9fe8aed7340b53faa0826177cdedbc11fff52907
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-20 20:51:11 -0700

    Add firebase config params

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyCyU1wEQGTikZL7EOCmOTl-TOc0W3hjqcM",
+  
+    authDomain: "my-workout-helper-87e06.firebaseapp.com",
+  
+    projectId: "my-workout-helper-87e06",
+  
+    storageBucket: "my-workout-helper-87e06.firebasestorage.app",
+  
+    messagingSenderId: "752985904295",
+  
+    appId: "1:752985904295:web:cd4ce9a230529697797782",
+  
+    measurementId: "G-RXLFQBFXH6"
+  
+  };
+  
